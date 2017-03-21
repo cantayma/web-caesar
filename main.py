2d9e9787a40c6cf512f6e9095f7571777503b831
Candice Marquart <candicemarquart@gmail.com>
2017-03-21 10:53:18 -0500

    Inserted Rot 13 helper function

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import webapp2
+import string
+
+#rot 13 helper function from Unit 1, Ch. 12, Ex. 10.
+def rot13(mess):
+
+    idx=0
+    codedText=""
+
+    while idx < len(mess):
+        if mess[idx] in string.ascii_lowercase:
+            cipheredLetter = string.ascii_lowercase[(string.ascii_lowercase.find(mess[idx]) + 13) % 26]
+            codedText += cipheredLetter
+            idx += 1
+
+        elif mess[idx] in string.ascii_uppercase:
+            cipheredLetter = string.ascii_uppercase[(string.ascii_uppercase.find(mess[idx]) + 13) % 26]
+            codedText += cipheredLetter
+            idx += 1
+
+        else:
+            codedText += mess[idx]
+            idx += 1
+
+    return codedText
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
