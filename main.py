c894d709ba5d33995e9a80f50cad0dd9fa5ccb85
Candice Marquart <candicemarquart@gmail.com>
2017-03-21 13:42:25 -0500

    moved encrypt, rotate_character, and alphabet_position to own file, caesar.py

--- main.py
+++ main.py
@@ -1,47 +1,5 @@
 import webapp2
-import string
-
-def alphabet_position(letter):
-    """
-    receives a letter, returns 0-based numerical position in the alphabet;
-    case-sensitive; assumes letters only input
-    """
-
-    if letter in string.ascii_uppercase:
-        position = string.ascii_uppercase.find(letter)
-
-    elif letter in string.ascii_lowercase:
-        position = string.ascii_lowercase.find(letter)
-
-    return position
-
-def rotate_character(char, rot):
-    """
-    recieves a string, char, and an integer, rot, and returns a new string
-    that is the character after it is rotated through the alphabet by rot
-    """
-
-    if char in string.ascii_uppercase:
-        newChar = string.ascii_uppercase[(alphabet_position(char) + rot) % 26]
-    elif char in string.ascii_lowercase:
-        newChar = string.ascii_lowercase[(alphabet_position(char) + rot) % 26]
-    else:
-        newChar = char
-
-    return newChar
-
-def encrypt(text, rot):
-    """
-    receives a string, text and an int, rot, and returns the encrypted text by
-    rotating each character in text by rot positions
-    """
-
-    encryptedText = ""
-
-    for aLetter in text:
-        encryptedText += rotate_character(aLetter, rot)
-
-    return encryptedText
+import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -49,7 +7,7 @@ class MainHandler(webapp2.RequestHandler):
         message = "Hello, World!"
         rot_value = 13
 
-        self.response.write(encrypt(message, rot_value))
+        self.response.write(caesar.encrypt(message, rot_value))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
