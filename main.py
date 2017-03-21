c8d0919e7431bb9508d65d6ea6caa5f171aaa5ab
Candice Marquart <candicemarquart@gmail.com>
2017-03-21 15:42:00 -0500

    Added form labels, cleaned up layout on get handlers

--- main.py
+++ main.py
@@ -4,18 +4,33 @@ import caesar
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
+        message_label = "<label>Type a message: </label>"
         text_area = "<textarea name='message'></textarea>"
-        submit_button = "<input type='submit' />"                                               #creates a button that, when it is clicked,causes the form to submit
-        form = "<form method='post' >" + text_area + "<br><br>" + submit_button + "</form>"     #ACTION attirbute tells us WHERE we submit the form to
+
+        rotation_label = "<label>Rotate by: </label>"
+        rotation_input = "<input type='number' name='rotation' />"
+        submit_button = "<input type='submit' />"
+
+        form = ("<form method='post' >"
+                    + rotation_label + rotation_input
+                    + "<br><br>"
+                    + message_label + text_area
+                    + "<br><br>"
+                    + submit_button
+                    + "</form>")
+
+                                #ACTION attirbute tells us WHERE we submit the form to
                                 #If there is no ACTION attirubte, the form sends an other http request to the same place it started
                                 #The METHOD attirbute is used here with the value, POST.
                                 #METHOD specifies which type of http request you want to send.
                                 #The default method is GET request.
                                 #Now that we have a POST REQUEST, we have set up a POST REQUEST HANDLER, other we get 405 error.
+        header = "<h2>Web Caesar</h2>"
 
-        self.response.write(form)
+        self.response.write(header + form)
 
     def post(self):
+        rotation = int(self.request.get("rotation"))
         message = self.request.get("message")       #self.request.get is an attirbute of RequestHandler that allows us to access the GET request
                                                     #in the case, the GET request is the text the user typed in, or the MESSAGE WE WANT TO ENCRYPT
                                                     #self.request.get TAKES ONE ARGUMENT that is the KEYNAME
@@ -23,8 +38,7 @@ class MainHandler(webapp2.RequestHandler):
                                                     #this made up key name corresponds to the VALUE the user inputs in the textarea
                                                     #the key-value pair needs to be associated both in the post and get methods using a name attribute in the textarea tag
 
-        rot_value = 13
-        encrypted_message = caesar.encrypt(message, rot_value)
+        encrypted_message = caesar.encrypt(message, rotation)
 
         self.response.write("Secret Message: " + encrypted_message)
 
